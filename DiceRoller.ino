db6ccc58a546e5cdbbda69fdd407cbc82476f778
Claude Luo <babooclaude@hotmail.com>
2014-12-15 13:02:42 -0500

    Modified to allow native compilation in Arduino

--- DiceRoller.ino
+++ DiceRoller.ino
@@ -8,10 +8,22 @@ const int random_seeder = A0;
 
 static int guess;
 
-// #define CONFIG_ENABLE_TRUE_RANDOM
+#define CONFIG_ENABLE_TRUE_RANDOM
 #define CONFIG_LEVEL_TRUE_RANDOM 0
 
-#include "Config.ino"
+#ifdef CONFIG_ENABLE_TRUE_RANDOM
+	#if CONFIG_LEVEL_TRUE_RANDOM == 0
+		#define SEED_RANDOM() (randomSeed(analogRead(A0 + A1 + A2 + A3 + A4 + A5)))
+	#elif CONFIG_LEVEL_TRUE_RANDOM == 1
+		#define SEED_RANDOM() (randomSeed(analogRead(A0 + A1 + A2)))
+	#elif CONFIG_LEVEL_TRUE_RANDOM == 2
+		#define SEED_RANDOM() (randomSeed(analogRead(A0)))
+	#else
+		#define SEED_RANDOM() (randomSeed(analogRead(A0 + A1 + A2 + A3 + A4 + A5)))
+	#endif
+#else
+	#define SEED_RANDOM()
+#endif
 
 void setup()
 {
